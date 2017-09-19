54b0040a8ee29c88c57da6099d4351116fb0e49f
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-09-19 17:32:33 +0100

    Added commit methods

--- src/MagicMeta/Traits/IsMagic.php
+++ src/MagicMeta/Traits/IsMagic.php
@@ -3,6 +3,77 @@
 	namespace WPKit\MagicMeta\Traits;
 	
 	trait IsMagic {
+		
+		/**
+	     * @var array
+	     */
+		protected $committed_meta = [];
+		
+		/**
+	     * Boot the model
+	     *
+		 */
+		public static function boot()
+	    {
+	        parent::boot();
+	
+	        static::saved(function($model)
+	        {
+	            foreach($model->getCommitedMeta() as $key => $meta_value) {
+		            $model->updateMetaValue( $model->getMagicMetaKey( $key ), $value );
+	            }
+	            $this->clearCommit();
+	        });
+	    }
+	    
+	    /**
+	     * Get the meta that has been commited for save
+	     *
+	     * @return array
+	     */
+	    public function getCommitedMeta() 
+	    {
+			return property_exists($this, 'committed_meta') ? $this->commited_meta : [];
+	    }
+	    
+	    /**
+	     * Commit meta for save
+	     *
+	     * @param string $key 
+	     * @param string $value
+	     * @return void
+	     */
+	    public function commitMeta($key, $value) 
+	    {
+			$this->commited_meta[$key] = $value;
+	    }
+	    
+	    /**
+	     * Clear commit meta for save
+	     *
+	     * @return void
+	     */
+	    public function clearCommit() 
+	    {
+			$this->commited_meta = [];
+	    }
+		
+		/**
+	     * Fill the model with an array of attributes.
+	     *
+	     * @param  array  $attributes
+	     * @return $this
+	     *
+	     * @throws \Illuminate\Database\Eloquent\MassAssignmentException
+	     */
+	    public function fill(array $attributes)
+	    {
+	        parent::fill($attributes);
+	        foreach($this->fillableFromMagicMeta($attributes) as $key => $value) {
+		        $this->commitMeta($key, $value);
+	        }
+	        return $this;
+	    }
 	
 	    /**
 	     * Get the magic meta of current instance
@@ -57,6 +128,17 @@
 	            $attributes[$key] = $this->getMeta($meta_key);
 	        }
 	        return $attributes;
+	    }
+	    
+	    /**
+	     * Get the commitable magic meta of a given array.
+	     *
+	     * @param  array  $attributes
+	     * @return array
+	     */
+	    public function fillableFromMagicMeta(array $attributes) 
+	    {
+		    return array_intersect_key($attributes, $this->getMagicMeta());
 	    }
 	
 	}
